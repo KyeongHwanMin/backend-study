b4f04f12f324f6e08f7797e9e629a9b156092cc0
KyeongHwan Min <h45652@naver.com>
2023-10-10 15:44:37 +0900

    FEAT : RecruitmentNoticeDetailView 생성

--- recruitment_notice/views.py
+++ recruitment_notice/views.py
@@ -1,3 +1,4 @@
+from django.shortcuts import get_object_or_404
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from recruitment_notice.models import Recruitment_Notice
@@ -16,3 +17,13 @@ class RecruitmentNoticeView(APIView):
             serializer.save()
             return Response(serializer.data, status=201)
         return Response(serializer.errors, status=400)
+
+
+class RecruitmentNoticeDetailView(APIView):
+    def get_object(self, pk):
+        return get_object_or_404(Recruitment_Notice, pk=pk)
+
+    def get(self, request, pk, format=None):
+        recruitment_notice = self.get_object(pk)
+        serializer = RecruitmentNoticeSerializer(recruitment_notice)
+        return Response(serializer.data)
