35133bc3fde96fbb85faf015d9e1c944c4423142
MinKyeongHwan <h45652@naver.com>
2023-10-10 21:15:39 +0900

    Update models.py

--- recruitment_notice/models.py
+++ recruitment_notice/models.py
@@ -2,13 +2,13 @@ from django.db import models
 from django.contrib.auth.models import User
 
 
-class Recruitment_Notice(models.Model):
+class RecruitmentNotice(models.Model):
     company = models.ForeignKey(to="company.Company", on_delete=models.CASCADE,
                                 related_name="recruitment_notices", help_text="회사 ID")
-    recruitment_potion = models.CharField(max_length=128, help_text="채용 포지션")
-    recruitment_compensation = models.IntegerField(help_text="채용 보상금")
-    recruitment_content = models.TextField(blank=True)
-    skill = models.CharField(max_length=128)
+    position = models.CharField(max_length=50, help_text="채용 포지션")
+    compensation = models.SmallIntegerField(default=0, help_text="채용 보상금")
+    content = models.TextField(blank=True)
+    skill = models.CharField(max_length=50)
     created_at = models.DateTimeField(auto_now_add=True, help_text="생성 날짜")
     modified_at = models.DateTimeField(auto_now=True, help_text="수정 날짜")
 
