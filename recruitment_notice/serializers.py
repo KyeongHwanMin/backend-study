04e23cc0ab914a1dd8fe302961bbfdfd19c68e15
KyeongHwan Min <h45652@naver.com>
2023-10-10 17:24:47 +0900

    FEAT : 채용공고 목록 가져오기 수정중

--- recruitment_notice/serializers.py
+++ recruitment_notice/serializers.py
@@ -1,16 +1,48 @@
-from rest_framework.serializers import ModelSerializer
-from recruitment_notice.models import Recruitment_Notice
+from company.models import Company
+from .models import Recruitment_Notice
+from rest_framework import serializers
+from company.serializers import CompanySerializer
 
 
-class RecruitmentNoticeSerializer(ModelSerializer):
+class RecruitmentNoticeSerializer(serializers.ModelSerializer):
+    company = CompanySerializer()
+
+    def create(self, validated_data):
+
+        company_data = validated_data.pop('company')
+        company_instance = Company.objects.get(**company_data)
+        recruitment_notice = Recruitment_Notice.objects.created(company=company_instance, **validated_data)
+
+        return recruitment_notice
+
     class Meta:
         model = Recruitment_Notice
-        fields = '__all__'
-        # fields = [
-        #     'company',
-        #     'recruitment_potion',
-        #     'recruitment_compensation',
-        #     'recruitment_content',
-        #     'skill',
-        #
-        # ]
+        fields = [
+            'company',
+            'recruitment_potion',
+            'recruitment_compensation',
+            'recruitment_content',
+            'skill',
+        ]
+
+# class RecruitmentNoticeSerializer(serializers.Serializer):
+#     company = serializers.PrimaryKeyRelatedField(queryset=Company.objects.all())  # 이 부분 변경
+#     recruitment_potion = serializers.CharField(max_length=128)
+#     recruitment_compensation = serializers.IntegerField()
+#     recruitment_content = serializers.CharField()
+#     skill = serializers.CharField(max_length=128)
+#
+#     def create(self, validated_data):
+#
+#         recruitment_notice = Recruitment_Notice.objects.create(**validated_data)
+#         return recruitment_notice
+#
+#     def update(self, instance, validated_data):
+#         instance.company = validated_data.get('company', instance.company)
+#         instance.recruitment_potion = validated_data.get('recruitment_potion', instance.recruitment_potion)
+#         instance.recruitment_compensation = validated_data.get('recruitment_compensation', instance.recruitment_compensation)
+#         instance.recruitment_content = validated_data.get('recruitment_content', instance.recruitment_content)
+#         instance.skill = validated_data.get('skill', instance.skill)
+#         instance.save()
+#         return instance
+
--- recruitment_notice/views.py
+++ recruitment_notice/views.py
@@ -13,7 +13,9 @@ class RecruitmentNoticeView(APIView):
         return Response(serializer.data)
 
     def post(self, request):
+
         serializer = RecruitmentNoticeSerializer(data=request.data)
+
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
