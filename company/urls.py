99dacd4a7d1ad000c7928760e4ca9518add03e51
KyeongHwan Min <h45652@naver.com>
2023-10-10 15:04:32 +0900

    FEAT : Company(model,serializer,view) 생성

--- company/urls.py
+++ company/urls.py
@@ -1,3 +1,6 @@
-urlpatterns = [
+from django.urls import path
+from company.views import CompanyView
 
+urlpatterns = [
+    path('', CompanyView.as_view()),
 ]
--- company/views.py
+++ company/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from rest_framework.response import Response
 
-# Create your views here.
+from company.models import Company
+from rest_framework.views import APIView
+from company.serializers import CompanySerializer
+
+
+class CompanyView(APIView):
+    def get(self, request):
+        qs = Company.objects.all()
+        serializer = CompanySerializer(qs, many=True)
+        return Response(serializer.data)
+    pass
