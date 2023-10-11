4fcb8aba604d0dfe5478a05b655400da42884de6
KyeongHwan Min <h45652@naver.com>
2023-10-11 17:57:46 +0900

    REFACTOR : 채용지원시 1번이상 지원하면 에러문구 출력

--- recruitment_support/views.py
+++ recruitment_support/views.py
@@ -9,7 +9,7 @@ from recruitment_support.serializers import RecruitmentSupportSerializer
 
 
 class RecruitementSupportView(APIView):
-    # permission_classes = [IsAuthenticated]
+    permission_classes = [IsAuthenticated]
     def get(self, request):
         qs = Support.objects.filter(user=request.user)
         serializer = RecruitmentSupportSerializer(qs, many=True)
@@ -21,7 +21,12 @@ class RecruitementSupportView(APIView):
         print(request.user)
         serializer = RecruitmentSupportSerializer(data=data)
 
+
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        errors = serializer.errors
+
+        if "user" in errors and str(errors["user"][0]) == "support with this user already exists.":
+            errors["user"] = ["이미 지원하셨습니다."]
+        return Response(errors, status=status.HTTP_400_BAD_REQUEST)
