cb12b21609ba46cc4220e409b3aadc8932142f3a
KyeongHwan Min <h45652@naver.com>
2023-10-11 09:57:14 +0900

    FEAT : account 커스텀 모델 생성

--- account/models.py
+++ account/models.py
@@ -1,3 +1,35 @@
 from django.db import models
+from django.contrib.auth.base_user import BaseUserManager, AbstractBaseUser
 
-# Create your models here.
+
+class UserManager(BaseUserManager):
+    def create_user(self, email, name, password=None):
+        if not email:
+            raise ValueError('이메일을 입력해 주세요.')
+        if not name:
+            raise ValueError('이름을 입력해 주세요.')
+        user = self.model(
+            email=self.normalize_email(email),
+            name=name,
+        )
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+
+class User(AbstractBaseUser):
+    id = models.AutoField(primary_key=True)
+    email = models.EmailField(default='', max_length=100, null=False, blank=False, unique=True)
+    name = models.CharField(default='', max_length=100, null=False, blank=False)
+
+    is_active = models.BooleanField(default=True)
+    is_admin = models.BooleanField(default=False)
+
+    objects = UserManager()
+
+    USERNAME_FIELD = 'name'
+
+    REQUIRED_FIELDS = ['email', 'name']
+
+    def __str__(self):
+        return self.name
